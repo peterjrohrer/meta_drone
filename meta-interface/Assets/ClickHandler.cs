ccc1050e4e1e2cf85bacf7e7bacf5ef94c42ddd2
mjkaufer <mjkaufer@gmail.com>
2015-01-17 08:36:49 -0500

    Unity files, .gitignore

---
+++ meta-interface/Assets/ClickHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickHandler : MonoBehaviour {
+
+	void OnEnable(){
+		Debug.Log ("Loaded");
+		//gameObject.renderer.material.color = Color.red;
+	}
+
+	void OnMouseDown(){
+		Debug.Log("Mouse down");
+	}
+
+}
